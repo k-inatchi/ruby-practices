505b8314f4f6c30318647b8de6fd3e6050f83d2c
inatchi <k.inatsu@pepabo.com>
2026-06-18 09:34:54 +0900

    why以外のコメントは削除

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
-array = (1..20).to_a  # 1から20までの配列を作成する
-1.upto(20) do |i| # 配列をループで処理
-  result = if (i % 3 == 0 && i % 5 == 0) # 両方の倍率に合致した場合 = 3でも5でも割り切れるときはFizzBuzzを返す
+array = (1..20).to_a
+1.upto(20) do |i|
+  result = if (i % 3 == 0 && i % 5 == 0)
     "FizzBuzz"
-  elsif (i % 3 == 0)  # 3の倍数の時 = 3で割り切れるときはFizzを返す
+  elsif (i % 3 == 0)
     "Fizz"
-  elsif (i % 5 == 0) # 5の倍数の時　= 5で割り切れるときはBuzzを返す
+  elsif (i % 5 == 0)
     "Buzz"
   else
-    i # それ以外はそのまま返す
+    i
   end
-  puts result # ifの返り値をプリント
+  puts result
 end
