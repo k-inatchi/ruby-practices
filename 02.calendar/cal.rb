94435771aab428a4a34ba2ed08f35c6c9e02172c
inatchi <k.inatsu@pepabo.com>
2026-06-21 15:17:51 +0900

    文字列をシングルクォートに統一した

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-require "date"
-require "optparse"
+require 'date'
+require 'optparse'
 
 opt = OptionParser.new
 params = {}
@@ -14,16 +14,16 @@ today = Date.today
 year = (params[:year] || today.year).to_i
 month = (params[:month] || today.month).to_i
 first_date = Date.new(year, month, 1)
-header = "#{first_date.strftime("%B")} #{year}"
-week = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"].join(" ")
+header = "#{first_date.strftime('%B')} #{year}"
+week = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'].join(' ')
 
 puts header.center(week.length)
 puts week
 wday = first_date.wday
-print (" " * DAY_WIDTH + " ") * wday
+print (' ' * DAY_WIDTH + ' ') * wday
 1.upto(Date.new(year, month, -1).day) do |i|
   wday = wday + 1
-  print i.to_s.rjust(DAY_WIDTH) + " "
+  print i.to_s.rjust(DAY_WIDTH) + ' '
   if wday % WEEK_DAYS == 0
     print "\n"
   end
