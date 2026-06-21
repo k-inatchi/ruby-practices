aa79c2bf32204963368490179c299d9f94405ccc
inatchi <k.inatsu@pepabo.com>
2026-06-21 15:10:12 +0900

    日付幅と週の日数のマジックナンバーを定数にした

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,6 +8,8 @@ opt.on('--year YEAR') {|v| params[:year] = v}
 opt.on('--month MONTH') {|v| params[:month] = v}
 opt.parse!(ARGV)
 
+DAY_WIDTH = 2
+WEEK_DAYS = 7
 today = Date.today
 year = (params[:year] || today.year).to_i
 month = (params[:month] || today.month).to_i
@@ -18,11 +20,11 @@ week = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"].join(" ")
 puts header.center(week.length)
 puts week
 wday = first_date.wday
-print "   " * wday
+print (" " * DAY_WIDTH + " ") * wday
 1.upto(Date.new(year, month, -1).day) do |i|
   wday = wday + 1
-  print i.to_s.rjust(2) + " "
-  if wday % 7 == 0
+  print i.to_s.rjust(DAY_WIDTH) + " "
+  if wday % WEEK_DAYS == 0
     print "\n"
   end
 end
