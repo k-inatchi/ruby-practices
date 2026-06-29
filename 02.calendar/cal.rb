5aae69aedaf9d7e527048372a077eaae9242e442
inatchi <k.inatsu@pepabo.com>
2026-06-29 10:18:57 +0900

    last_dateが複数回必要になったので変数にした

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -19,6 +19,7 @@ today = Date.today
 year = (params[:year] || today.year).to_i
 month = (params[:month] || today.month).to_i
 first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
 header = "#{month}月 #{year}"
 week = %w(日 月 火 水 木 金 土).join(' ')
 
@@ -26,11 +27,11 @@ puts header.center(display_width(week))
 puts week
 print (' ' * DAY_WIDTH + ' ') * first_date.wday
 
-first_date.upto(Date.new(year, month, -1)) do |target_date|
+first_date.upto(last_date) do |target_date|
   print target_date.day.to_s.rjust(DAY_WIDTH) + ' '
     if target_date.saturday?
     print "\n"
   end
 end
 
-puts unless Date.new(year, month, -1).saturday?
+puts unless last_date.saturday?
