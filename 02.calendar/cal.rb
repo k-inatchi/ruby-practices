d9a3bb6ef24912237cb31806f5c49b62dcdc3e12
inatchi <k.inatsu@pepabo.com>
2026-07-05 12:56:27 +0900

    半角空白を意味のある変数名に置き換える

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -18,22 +18,20 @@ DAY_WIDTH = 2
 EMPTY_DAY = ' ' * DAY_WIDTH
 DAY_SPACER = ' '
 
-
 today = Date.today
 year = (params[:year] || today.year).to_i
 month = (params[:month] || today.month).to_i
 first_date = Date.new(year, month, 1)
 last_date = Date.new(year, month, -1)
 header = "#{month}月 #{year}"
-week = %w[日 月 火 水 木 金 土].join(' ')
+week = %w[日 月 火 水 木 金 土].join(DAY_SPACER)
 
 puts header.center(display_width(week))
 puts week
 print (EMPTY_DAY + DAY_SPACER) * first_date.wday
 
-
 first_date.upto(last_date) do |target_date|
-  print target_date.day.to_s.rjust(DAY_WIDTH) + ' '
+  print target_date.day.to_s.rjust(DAY_WIDTH) + DAY_SPACER
   print "\n" if target_date.saturday?
 end
 
